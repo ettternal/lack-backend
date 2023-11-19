7e58e74300af667324016d55cde1edb04cae41fb
qhyang <2194784330@qq.com>
2023-11-19 12:40:46 +0800

    前置项目-用户中心完成阶段

---
+++ src/main/java/com/whale/usercenter/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.whale.usercenter.mapper;
+
+import com.whale.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
